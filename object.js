28cf784941cb5924a4118d714725ee818401dcef
root <root@localhost.localdomain>
2018-05-16 22:34:02 +0700

    'belajar 'array','nested array','objects','array dalam object'

---
+++ object.js
@@ -0,0 +1,14 @@
+function belajarJavascriptObject(){
+   var friend = {
+      name: 'Abdul',
+      nickname:   'Dul',
+      age:  '21',
+      address: 'Payakumbuh',
+   }
+
+   console.log(friend)
+   console.log(friend.nickname)
+   console.log(friend.address)
+}
+
+belajarJavascriptObject()
