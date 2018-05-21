3e2e971328e4baf3b0efbdf6f75de44e145810ae
root <root@localhost.localdomain>
2018-05-21 23:21:38 +0700

    check data type on javascript

---
+++ typeof.js
@@ -0,0 +1,16 @@
+function checkDataType(){
+  var name = "Tri"
+  var height = 173
+  var bio = {
+    name: 'Tri',
+    age: '22'
+  }
+  var grocery_lists = ['Orange', 'Mangosteen']
+
+  console.log(typeof(name))
+  console.log(typeof(height))
+  console.log(typeof(bio))
+  console.log(typeof(grocery_lists))
+}
+
+checkDataType()
