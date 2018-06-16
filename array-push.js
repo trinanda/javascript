13c0ac12b1a1448a9fc18dbef065084ea776ef0f
root <root@localhost.localdomain>
2018-06-17 05:20:35 +0700

    belajar operator, if else berdasarkan tipe dat,  belajar array-shift, array-pop, array-push

---
+++ array-push.js
@@ -0,0 +1,11 @@
+function arrayPush(){
+  var pelajaran = [1,5,4,2,6,7,3,2];
+  console.log(pelajaran);
+
+  pelajaran.push(100);
+  pelajaran.push('lima ratus');
+
+  return pelajaran;
+}
+
+console.log(arrayPush());
