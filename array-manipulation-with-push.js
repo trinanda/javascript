7074d402a1410bdf0defadee3e1e781e2cd257a2
root <root@localhost.localdomain>
2018-09-05 15:27:27 +0700

    manipulate array with push

---
+++ array-manipulation-with-push.js
@@ -0,0 +1,10 @@
+function manipulationArrayWithPush(){
+  var kucing = ['Lucky', 'Gofy', 'Kitty']
+  console.log(kucing)
+
+  kucing.push('Sijoy', 'Sitoy')
+
+  return kucing;
+}
+
+console.log(manipulationArrayWithPush());
