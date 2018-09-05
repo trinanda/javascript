815226fecb75a1c79084eceef20f04c28718d09e
root <root@localhost.localdomain>
2018-09-05 11:50:34 +0700

    belajar objects javascript

---
+++ objects.js
@@ -0,0 +1,13 @@
+function belajarObjects(){
+  var kucing = {
+    nama: 'Kitty',
+    warna: 'abu-abu',
+    jenisKelamin: 'Perempuan'
+  }
+  console.log(kucing)
+  console.log(kucing.nama)
+  console.log(kucing.warna)
+}
+
+
+belajarObjects()
