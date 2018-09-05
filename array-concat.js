8d06daa549b60c12c60c499223ad130debe9040e
root <root@localhost.localdomain>
2018-09-05 19:20:26 +0700

    concat untuk extends array

---
+++ array-concat.js
@@ -0,0 +1,10 @@
+function extendsArray(){
+  var ternak = ['Kambing', 'Kerbau', 'Belut']
+  var tanaman = ['Manggis', 'Jeruk', 'Jagung']
+
+  var gabungKan = tanaman.concat(ternak)
+
+  return gabungKan
+}
+
+console.log(extendsArray());
