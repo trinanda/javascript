657ed99decc085580749424894a1c51190a75733
root <root@localhost.localdomain>
2018-09-05 10:43:06 +0700

    belajar array javascript

---
+++ array.js
@@ -0,0 +1,12 @@
+function belajarArray(){
+  var nomorBt = [3,5,2,6,7,1]
+  var kota = ['Jakarta', 'Bogor', 'Yogyakarta', 'Makasar']
+
+  console.log(nomorBt.length)
+  console.log(kota.length)
+
+  console.log(nomorBt[4])
+  console.log(kota[3])
+}
+
+belajarArray()
