f06290e0c8d48789aa812396aef049023b04966d
root <root@localhost.localdomain>
2018-09-05 07:21:01 +0700

    mengulang pelajaran javascript dari console.log

---
+++ consoleLog.js
@@ -0,0 +1,7 @@
+var message1 = 'Belajar console log dengan variable var'
+let message2 = 'Belajar console log dengan variable let'
+const message3 = 'Belajar console log dengan variable const'
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
