4ecfe8d3227e97067568fd984fa0014658385115
root <root@localhost.localdomain>
2018-09-05 21:00:38 +0700

    reverse berfungsi untuk membalikan element pada array

---
+++ array-reverse.js
@@ -0,0 +1,9 @@
+// seperti nama fungsi nya sendiri, reverse berfungsi untuk membalikan element pada array
+function membalikanElementPadaArray() {
+  var urutanAngka = ['satu', 'dua', 'tiga', 'empat']
+  console.log(urutanAngka);
+
+  return urutanAngka.reverse()
+}
+
+console.log(membalikanElementPadaArray());
