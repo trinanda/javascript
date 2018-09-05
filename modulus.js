18aeff42f54a90c4543db33c95187cda9cb56d17
root <root@localhost.localdomain>
2018-09-05 21:17:46 +0700

    modulus berfungsi untuk mencari hasil sisa pembagian antara pembagian bilangan

---
+++ modulus.js
@@ -0,0 +1,8 @@
+// modulus berfungsi untuk mencari sebuah sisa pembagian antara beberapa bilangan
+function cariSisaPembagian() {
+  var angka1 = 38
+  var angka2 = 3
+  return angka1 % angka2
+}
+
+console.log(cariSisaPembagian());
