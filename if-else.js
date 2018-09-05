5059eea830e5a131a9ba017b5460e2cb01b72246
root <root@localhost.localdomain>
2018-09-05 09:16:01 +0700

    belajar if else javascript

---
+++ if-else.js
@@ -0,0 +1,18 @@
+function kompas(){
+  arah = 'kanan1'
+
+  if(arah === 'kanan'){
+    console.log('Barat')
+  }
+  else if(arah === 'Kiri'){
+    console.log('Timur')
+  }
+  else{
+    console.log('Tidak tahu')
+  }
+}
+
+kompas()
+
+if(arah==='Kanan')console.log('Barat')
+else console.log('Invalid!!!')
