5561ef2dcc6a7b2661a756212739889cce491559
root <root@localhost.localdomain>
2018-09-05 13:04:48 +0700

    how to check data type on javascript

---
+++ checkDataType.js
@@ -0,0 +1,17 @@
+function checkDataType(){
+  var name = 'Kitty'
+  var age = 8
+  var book = ['Python for Beginner', 'Javascript for Dummies', 'PostgreSQL for Expert']
+  var company = {
+    name: 'Facebook',
+    founder: 'Mark Zuckerberg',
+    age: 34
+  }
+
+  console.log(typeof(name))
+  console.log(typeof(age))
+  console.log(typeof(book))
+  console.log(typeof(company))
+}
+
+checkDataType()
