993d4e88f1cc484fb29b250588a8e8b9d9cd6b0c
root <root@localhost.localdomain>
2018-09-05 11:06:57 +0700

    belajar nested array

---
+++ nested-array.js
@@ -0,0 +1,15 @@
+function belajarNestedArray(){
+  var kota = [
+    ['Duri', 'Pekanbaru', 'Langgam'],
+    ['Bogor', 'Sumedang'],
+    ['Fak Fak', 'Jayapura', 'Merauke']
+  ]
+
+  console.log(kota)
+  console.log('------------');
+  console.log(kota[0])
+  console.log('------------');
+  console.log(kota[2][1]);
+}
+
+belajarNestedArray()
