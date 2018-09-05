001ae48254e6ca0d5b22c3ddbfcdc17080c266e2
root <root@localhost.localdomain>
2018-09-05 08:24:03 +0700

    belajar cara penulisan function javascript

---
+++ functions.js
@@ -0,0 +1,19 @@
+function satu(){
+  console.log('testing function satu');
+}
+
+const dua = function(){
+  console.log('testing function dua');
+}
+
+satu()
+dua()
+
+// pada dasar nya kedua function di atas sama, hanya metode penulisan nya yang berbeda
+
+// Dan yang dibawah ini adalah cara penulisan function yang baru
+const tiga = ()=>{
+  console.log('testing function tiga');
+}
+
+tiga()
