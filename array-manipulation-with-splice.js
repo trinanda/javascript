a012b13b431932172b90d15f1f1ea8dcb6f64b39
root <root@localhost.localdomain>
2018-09-05 17:57:30 +0700

    mengetahui beberapa fungsi dari splice

--- array-manipulation-with-splice.js
+++ array-manipulation-with-splice.js
@@ -1,8 +1,11 @@
+// splice memiliki beberapa fungsi, yaitu untuk menyisipkan data dan menghapus data
 function manipulationArrayWithSplice(){
   var hewan = ['gajah', 'harimau', 'rusa', 'badak', 'beruang']
   console.log(hewan)
 
-  hewan.splice(3, 0, 'semut')
+  // hewan.splice(3, 0, 'semut')          // menyisipkan data
+  // hewan.splice(3)                      // menyisakan 4 element yang di hitung dari index 1
+  hewan.splice(1,2)                       // menghapus index ke 2 data yang di mulai dari index ke 1
 
   return hewan
 }
