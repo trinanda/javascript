48737bdfa2b3b1f6bcb86ed8a2ed0406220f515a
root <root@localhost.localdomain>
2018-09-09 16:16:10 +0700

    parameters adalah sebutan untuk inputan pada saat pendefenisian function

---
+++ parameterOnFunction.js
@@ -0,0 +1,12 @@
+/*
+    - parameter adalah sebutan untuk inputan fungsi pada saat pendefenisian fungsi tersebut
+    - Dalam sebuah function kita boleh memasukan lebih dari 1 buah parameter
+    - Jenis tipe data yang bisa di tampun oleh parameter juga bebas sesuai dengan kebutuhan
+*/
+
+function whatIsTheParameters(streetNumber, city, country){
+  console.log(streetNumber);
+  console.log(city);
+  console.log(country);
+}
+whatIsTheParameters(1123, ['Payakumbuh','Padang','Bukit Tinggi'], 'Indonesia');
