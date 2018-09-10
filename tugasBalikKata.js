3e04e0d642269ffbf9b7f6c108093b521c953d4c
root <root@localhost.localdomain>
2018-09-10 17:02:54 +0700

    masih belum paham cara mengerjakan tugas ini

---
+++ tugasBalikKata.js
@@ -0,0 +1,10 @@
+function balikKata(kata) {
+  // tulis jawaban disini
+  
+}
+
+// testCase
+console.log(balikKata('Niomic!'));
+console.log(balikKata('Javascript'));
+console.log(balikKata('alohahola'));
+console.log(balikKata('Sumatera_Barat'));
