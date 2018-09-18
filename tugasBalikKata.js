aec493fa7d987f6fa1af0cf56d116a750cc62d6b
root <root@localhost.localdomain>
2018-09-18 17:04:23 +0700

    tugas balik kata sudah selesai, namun pemahaman kenapa bisa berhasil masih kurang

--- tugasBalikKata.js
+++ tugasBalikKata.js
@@ -1,24 +1,42 @@
-function balikKata(kata) {
-  // jawaban:
-  // return kata.split('').reverse().join('')  // tidak boleh menggunakan dari ketiga method ini
-  balikKan = [];    // membuat sebuah variable array kosong untuk menampung data nanti nya
-  // untuk iterasi i = 0; jika nilai i masih kecil dari panjang ('nilai yang di dimasukan ke dalam parameter "kata"'); tambahkan terus nilai i;
-  for (i = 0; i < kata.length; i++) {
-    balikKan = kata[i] + balikKan;   // cara 1. (cara ini saya copas dari stackoferflow.com (https://stackoverflow.com/a/51751393/8070090), namun tidak memiliki keterangan yang jelas)// saya tidak mengerti kenapa pada baris ini bisa membalikkan kata
-      // balikKan = kata[i] + [];    // pertama saya mengganggap bahwa nilai dari variable "balikKan" adalah sama dengan ini "[]", tapi ternyata baris ini tidak sama dengan hasil di atas
-      // balikKan = kata[i] + '';    // dan kemudian saya mencoba cara ini dengan menganggap bahwa nilai dari variable "balikKan" adalah string kosong atau (""), tapi ternyata cara ini tetap tidak menghasilkan hasil yang diharapkan
-
-      // var testing = [];             // saya mencoba membuat array kosong lagi
-      // balikKan = kata[i] + testing; // dan mencoba menambahkan array kosong tersebut pada variable ini (balikKan), tapi hasil nya masih tidak sama
-    /*
-      Jadi pertanyaaan nya, kenapa cara 1. tersebut bisa berhasil..?
-    */
+// // dengan for loop
+// function balikKata(kata) {
+//   var reverseIt = '';
+//   for (i = 0; i < kata.length; i++) {
+//     reverseIt = kata[i] + reverseIt;
+//   }
+//   return reverseIt;
+// }
+//
+// console.log(balikKata('Jakarta'));
+// console.log(balikKata('Bandung'));
+// console.log(balikKata('Yogyakarta'));
+// console.log(balikKata('Padang'));
+/*
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+*/
+// // dengan forEach   // yang ini masih menggunakan method "split"
+// function balikKata2(kata) {
+//   reverseIt = '';
+//   kata = kata.split('');
+//   kata.forEach(function(item, index, array) {
+//     reverseIt = item + reverseIt;
+//   });
+//   return reverseIt;
+// }
+// console.log(balikKata2('Sumedang'));
+/*
+-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
+*/
+function balikKata3(kata) {
+  var reverseIt = '';
+  var i = 0;
+  while (i < kata.length) {
+    reverseIt = kata[i] + reverseIt;
+    console.log(reverseIt);
+    i+=1;
   }
-  return balikKan;  // kembalikan nilai dari variable
+  return reverseIt;
 }
-
-// testCase
-console.log(balikKata('Niomic!'));
-console.log(balikKata('Javascript'));
-console.log(balikKata('alohahola'));
-console.log(balikKata('Sumatera_Barat'));
+balikKata3('testing')
