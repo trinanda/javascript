45fbe05c49484d9a86a1761089ee23ac426ad39b
root <root@localhost.localdomain>
2018-09-19 21:34:47 +0700

    dengan" flags g" akan menghasilkan atau mencetak huruf/kata yang di cari atau di cocok kan, jadi bukan hanya index lagi

--- regexp_flags_g.js
+++ regexp_flags_g.js
@@ -1,4 +1,5 @@
 // flags "g" berfungsi untuk mencari data yang sama di semua string yang kita jadikan acuan untuk mencocokkan data tersebut
+// ini akan mencetak huruf/kata yang di cari tersebut, jadi bukan berdasarkan index lagi
 function belajarRegexpFlags_G(){
   let dataHuruf = 'abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz';
 
