32b5dd54c9a98295372757db70a0ee16a7bb1416
root <root@localhost.localdomain>
2018-09-19 21:30:29 +0700

    tanpa "flags" hanya akan mengecek kata berdasarkan index yang paling pertama

--- regexp.js
+++ regexp.js
@@ -1,4 +1,5 @@
 // regular expresion berfungsi untuk mencari atau mengganti pada sebuah data string
+// ini masih tanpa "flags", jadi hanya akan di cek berdasarkan index paling pertama yang di temukan
 function belajarRegeXP() {
   let dataHuruf = 'abcdefghijklmnopqrstuvwxyz';
 
