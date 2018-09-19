d6aac9cf281962caabe443877c6097401f42beb3
root <root@localhost.localdomain>
2018-09-19 21:32:59 +0700

    dengan "flags i" bisa membuat pencarian menjadi case-insensitive, namun yang di cek tetap lah hanya index yang paling pertama ditemukan

--- regexp_flags.js
+++ regexp_flags.js
@@ -1,6 +1,7 @@
 // flags "i" berfungsi untuk membuat pencarian kata menjadi case-insensitive
+// ini juga hanya akan mengecek kata berdasarkan index yang paling pertama ditemukan
 function belajarRegexpFlags(){
-  let dataHuruf = 'abcdefghijklmnopqrstuvwxyz';
+  let dataHuruf = 'abcdefghijklmnopqrsktuvkwxyzk';
 
   console.log(dataHuruf.search(/K/));
   console.log(dataHuruf.search(/K/i));
