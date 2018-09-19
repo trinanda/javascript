9e79f89b30109adeb086b0452c2a65522af8a0c6
root <root@localhost.localdomain>
2018-09-19 15:11:16 +0700

    regular expresion berfungsi untuk mencari atau mengganti pada sebuah data string

---
+++ regexp.js
@@ -0,0 +1,8 @@
+// regular expresion berfungsi untuk mencari atau mengganti pada sebuah data string
+function belajarRegeXP() {
+  let dataHuruf = 'abcdefghijklmnopqrstuvwxyz';
+
+  return dataHuruf.search(/e/)
+}
+
+console.log(belajarRegeXP());
