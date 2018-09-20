ad62dcb242c15904b61b40cc928fe5cea9def5d7
root <root@localhost.localdomain>
2018-09-20 10:56:15 +0700

    mengerjakan tugas regexp, sudah berhasil ddengan cara yang di cari sendiri, namun masih belum efisien karena kepanjangan, jadi kode yang ini masih minta tolong sama "Team Pengajar Niomic "

--- tugas_regexp.js
+++ tugas_regexp.js
@@ -1,11 +1,11 @@
 // mengerjakan tugas ini masih minta bantuak sama "Team Pengajar Niomic"
-function taskRegeXP(value) {
+function tugasRegexp(value) {
   let str = "abcdefghijklmnopqrstuvwxyz-A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z";
 
   console.log(str.match(/c/ig));
-  console.log(str.match(/k/ig));
-  console.log(str.match(/l/ig));
+  console.log(str.match(/K/gi));
+  console.log(str.match(/L/ig));
 
 }
 
-taskRegeXP();
+tugasRegexp();
