63a106c9e18a32dba317d6b844243c1651a5a40d
root <root@localhost.localdomain>
2018-09-20 11:32:48 +0700

    mengerjaakan tugas replace regexp, namun masih belum bisa solved sendiri, dan akhir nya mencari referensi di stackoverflow.com

---
+++ replace_regexp_dan_tugas.js
@@ -0,0 +1,15 @@
+// method replace berfungsi untuk mengganti kata/data pada regexp
+// function panggilREgeXp() {
+//   let data = 'Belajar Satu Tahun berapa Niomic';
+//   console.log(data.replace(/berapa/,'Bersama'));
+// }
+
+// panggilREgeXp();
+
+// tugas
+// ketika mengerjakan tugas ini masih belum bisa solved sendiri, jadi masih mencari referensi di stackoverflow.com
+function taskRegexpReplace() {
+  let data = "Belajar-Satu-Tahun-Bersama-Niomic";
+  console.log(data.replace(/-/g,' '));
+}
+taskRegexpReplace();
